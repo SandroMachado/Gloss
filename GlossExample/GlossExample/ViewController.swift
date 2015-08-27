0b5305eb525025049b6270360a85c99fecb36434
Sandro Machado <sandroemachado@gmail.com>
2015-08-27 22:47:36 +0100

    Add travis integration

--- GlossExample/GlossExample/ViewController.swift
+++ GlossExample/GlossExample/ViewController.swift
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
         print(repo.url)
         print(repo.owner)
         print(repo.primaryLanguage?.rawValue)
-        print("", appendNewline: false)
+        print("", terminator: "\n")
         
         print("JSON: \(repo.toJSON())")
     }
